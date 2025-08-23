2d58cb8ee7ceaa6ca7a84a3c0f5893c11d401a5e
reirinaaa <reirinaa@proton.me>
2025-08-23 19:11:43 +0700

    m

--- main.cpp
+++ main.cpp
@@ -1,8 +1,69 @@
 #include <iostream>
 
+#include "Ogre.h"
+#include "OgreApplicationContext.h"
+
+class KeyHandler : public OgreBites::InputListener
+{
+    bool keyPressed(const OgreBites::KeyboardEvent& evt) override
+    {
+        if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
+        {
+            Ogre::Root::getSingleton().queueEndRendering();
+        }
+        return true;
+    }
+};
+
 int main()
 {
-    std::cout << "test" << std::endl;
+    std::cout << "start" << std::endl;
 
-    return  0;
+    OgreBites::ApplicationContext ctx("OgreTutorialApp");
+    ctx.initApp();
+
+    //! [setup]
+    // get a pointer to the already created root
+    Ogre::Root *root = ctx.getRoot();
+    Ogre::SceneManager *scnMgr = root->createSceneManager();
+
+    // register our scene with the RTSS
+    Ogre::RTShader::ShaderGenerator *shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+    shadergen->addSceneManager(scnMgr);
+
+    // without light we would just get a black screen
+    Ogre::Light *light = scnMgr->createLight("MainLight");
+    Ogre::SceneNode *lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    lightNode->setPosition(0, 10, 15);
+    lightNode->attachObject(light);
+
+    // also need to tell where we are
+    Ogre::SceneNode *camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    camNode->setPosition(0, 0, 15);
+    camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+
+    // create the camera
+    Ogre::Camera *cam = scnMgr->createCamera("myCam");
+    cam->setNearClipDistance(5); // specific to this sample
+    cam->setAutoAspectRatio(true);
+    camNode->attachObject(cam);
+
+    // and tell it to render into the main window
+    ctx.getRenderWindow()->addViewport(cam);
+
+    // finally something to render
+    Ogre::Entity *ent = scnMgr->createEntity("Sinbad.mesh");
+    Ogre::SceneNode *node = scnMgr->getRootSceneNode()->createChildSceneNode();
+    node->attachObject(ent);
+    //! [setup]
+
+    //! [main]
+    // register for input events
+    KeyHandler keyHandler;
+    ctx.addInputListener(&keyHandler);
+
+    ctx.getRoot()->startRendering();
+    ctx.closeApp();
+
+    return 0;
 }
